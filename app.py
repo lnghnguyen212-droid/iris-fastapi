5432ab118ccae0a68bb79d5f4e900ef33ddabb48
lnghnguyen212-droid <lnghnguyen212@gmail.com>
2026-09-18 07:54:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-import joblib
+import joblib 
 
 # Nạp mô hình SVM đã huấn luyện
 model = joblib.load("svm_model.pkl") 
